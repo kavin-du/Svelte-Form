73e30cd9dd05aca1e62662bae855fad8a1f9d8c8
Kavindu Chamith <kavindu.chamith@pagero.com>
2022-04-26 20:05:14 +0530

    close form after adding data

---
+++ src/AddPersonForm.svelte
@@ -0,0 +1,51 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  // we cannot use event forwarding
+  // bcz it doesnt send the data
+  let dispatch = createEventDispatcher();
+
+  let name;
+  let color;
+  let age;
+
+  let skills = [];
+
+  const handleSubmit = () => {
+    const person = {
+      name, 
+      color, 
+      age,
+      skills,
+      id: Math.random(), 
+    };
+
+    dispatch('addPerson', person);
+  };
+</script>
+
+<!-- submit event autmatically bind to any button in the form -->
+<!-- if not prevdef, page refresh, bcz that is the default action -->
+<form on:submit|preventDefault={handleSubmit}>
+  <input type="text" placeholder="name" bind:value={name} />
+  <input type="number" placeholder="age" bind:value={age} />
+
+  <label>Skills:</label>
+  <input type="checkbox" bind:group={skills} value="fighting" />fighting<br />
+  <input type="checkbox" bind:group={skills} value="sneaking" />sneaking<br />
+  <input type="checkbox" bind:group={skills} value="running" />running<br />
+
+  <label>Belt color:</label>
+  <select bind:value={color}>
+    <option value="black">black</option>
+    <option value="orange">orange</option>
+    <option value="brown">brown</option>
+    <option value="white">white</option>
+  </select>
+
+  <br />
+  <button>Add person</button>
+</form>
+
+<style>
+</style>
--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,72 @@
 <script>
-	export let name;
+  import Modal from "./Modal.svelte";
+  import AddPersonForm from "./AddPersonForm.svelte";
+
+  let showModal = false;
+
+  let people = [
+    { id: 1, name: "first", color: "red" },
+    { id: 2, name: "second", color: "blue" },
+    { id: 3, name: "third", color: "green" },
+  ];
+
+  const handleDelete = (id) => {
+    people = people.filter((person) => person.id !== id);
+  };
+
+  const toggleModal = () => {
+    showModal = !showModal;
+  };
+
+	const addPersonHandler = (e) => {
+		const person = e.detail;
+		// cannot do people.push(person)
+		// bcz we have to reassign in svelte
+
+		people = [...people, person];
+		showModal = false;
+	}
 </script>
 
+<Modal {showModal} on:click={toggleModal}>
+	<!-- custom event name then, handler name -->
+	<AddPersonForm on:addPerson={addPersonHandler} />
+</Modal>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <button on:click|once={toggleModal}>Show modal</button>
+  {#each people as p (p.id)}
+    <div>
+      <h4>{p.name}</h4>
+      <p>favorite color is {p.color}</p>
+      {#if p.color === "blue"}
+        <p><strong>BLUE FAVORITE</strong></p>
+      {/if}
+      <button on:click={() => handleDelete(p.id)}>delete</button>
+    </div>
+  {:else}
+    <p>all are empty</p>
+  {/each}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
